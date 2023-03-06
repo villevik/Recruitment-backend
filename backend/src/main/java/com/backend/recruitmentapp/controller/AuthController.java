f538b67c7d4641773fdf344f737c8ef976a68f19
Ville Vik <59703108+villevik@users.noreply.github.com>
2023-03-06 16:32:58 +0100

    Update AuthController.java

--- backend/src/main/java/com/backend/recruitmentapp/controller/AuthController.java
+++ backend/src/main/java/com/backend/recruitmentapp/controller/AuthController.java
@@ -82,7 +82,7 @@ public class AuthController {
         cookie.setPath("/"); // set the cookie's path to "/"
         cookie.setHttpOnly(true); // set the HttpOnly flag to prevent client-side access
         cookie.setSecure(true); // set the Secure flag to ensure the cookie is only sent over HTTPS
-        cookie.setSameSite("None"); // set the SameSite attribute to "None"
+        cookie.setComment("SameSite=None"); // set the SameSite attribute to "None"
 
         // set the cookie's max age to 1 hour
         Instant expirationTime = Instant.now().plus(Duration.ofHours(1));
