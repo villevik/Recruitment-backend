9ddab851877616e855627d2e4b54e02ce6b42c5d
Ville Vik <59703108+villevik@users.noreply.github.com>
2023-03-06 15:50:14 +0100

    Update WebSecurityConfig.java

--- backend/src/main/java/com/backend/recruitmentapp/security/WebSecurityConfig.java
+++ backend/src/main/java/com/backend/recruitmentapp/security/WebSecurityConfig.java
@@ -92,7 +92,7 @@ http.cors().and().csrf().disable()
    @Bean
    CorsConfigurationSource corsConfigurationSource() {
       CorsConfiguration configuration = new CorsConfiguration();
-      configuration.setAllowedOrigins(Arrays.asList("https://recruitmentappg10.azurewebsites.net/", "https://spiffy-mochi-c45fa0.netlify.app", "https://recruitment-frontend-henna.vercel.app/"));
+      configuration.setAllowedOrigins(Arrays.asList("https://recruitmentappg10.azurewebsites.net", "https://spiffy-mochi-c45fa0.netlify.app", "https://recruitment-frontend-henna.vercel.app"));
       configuration.setAllowedMethods(Arrays.asList("*"));
       configuration.setAllowedHeaders(Arrays.asList("*"));
       configuration.setAllowCredentials(true);
