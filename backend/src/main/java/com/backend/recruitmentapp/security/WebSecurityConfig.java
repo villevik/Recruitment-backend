d73d2124b5cb754c7c05e5f016076cc44023200e
Chouster-KTH <chouster@kth.se>
2023-03-09 00:08:53 +0100

    Update WebSecurityConfig.java

--- backend/src/main/java/com/backend/recruitmentapp/security/WebSecurityConfig.java
+++ backend/src/main/java/com/backend/recruitmentapp/security/WebSecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -18,7 +17,10 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
-
+/**
+ * This class is the crux of our security implementation.
+ * It configures cors, csrf, session management, rules for protected resources.
+ */
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(
@@ -53,20 +55,6 @@ public PasswordEncoder passwordEncoder() {
 //return new BCryptPasswordEncoder(); //Encrypted
 return new PasswordEnconderTest(); //No encryption
 }
-/*
-@Override
-protected void configure(HttpSecurity http) throws Exception {
-
-http.cors().and().csrf().disable()
-        .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
-        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-        .authorizeRequests().antMatchers("/api/auth/**").permitAll()
-        .antMatchers("/api/test/**").permitAll()
-        .anyRequest().authenticated();
-
-   http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-
-}*/
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
